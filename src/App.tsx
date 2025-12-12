1a1d48dc0f9eb5a1c5971e889b574466a67819a5
Luis Fernando Chumbes Ramos <luisfernando3chr@gmail.com>
2025-12-11 22:47:07 -0500

    Feat: Implementación del layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import MainLayout from './components/layout/MainLayout';
+import Home from './pages/Home';
+import Resume from './pages/Resume';
 
 function App() {
-
   return (
-    <>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        {/* Ruta Padre: Aplica el MainLayout a todo lo que esté dentro */}
+        <Route path="/" element={<MainLayout />}>
+          
+          {/* Index Route: Es lo que se ve en "/" */}
+          <Route index element={<Home />} />
+          
+          {/* Ruta CV: Se ve en "/cv", pero mantiene el fondo y navbar */}
+          {/* OJO: Si quieres el CV limpio para imprimir sin Navbar, 
+              lo sacarías fuera de este Route padre */}
+          <Route path="cv" element={<Resume />} />
+          
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
