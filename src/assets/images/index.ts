9f2e1b4ccdeba99834234bdb522f06d4885b1911
Luis Fernando Chumbes Ramos <luisfernando3chr@gmail.com>
2025-12-12 09:48:52 -0500

    Feat: index de assets

---
+++ src/assets/images/index.ts
@@ -0,0 +1,9 @@
+export { default as ProfileImage } from './profile.png';
+
+// import LoginBg from './login-bg.jpg';
+// import RegisterBg from './register-bg.jpg';
+
+// export const Backgrounds = {
+//   Login: LoginBg,
+//   Register: RegisterBg,
+// };
