b22a8a0740db0c5c5299526e9ea283497054bd41
Luis Fernando Chumbes Ramos <luisfernando3chr@gmail.com>
2025-12-17 17:18:27 -0500

    Feat: Se agrego logo a la web

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,4 +1,5 @@
 export { default as ProfileImage } from './profile.png';
+export { default as LogoImage } from './logo.png';
 
 // import LoginBg from './login-bg.jpg';
 // import RegisterBg from './register-bg.jpg';
