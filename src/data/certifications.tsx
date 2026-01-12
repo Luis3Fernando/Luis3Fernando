ccca2b1a55cd49a0bea21074574a00b4dc8c663f
Luis Fernando Chumbes Ramos <luisfernando3chr@gmail.com>
2026-01-11 22:04:41 -0500

    Feat: Cretificaciones completadas

--- src/data/certifications.tsx
+++ src/data/certifications.tsx
@@ -1,4 +1,5 @@
-import { FaAws, FaReact } from 'react-icons/fa';
+import { FaDocker } from 'react-icons/fa';
+import {SiDjango, SiSpring} from 'react-icons/si';
 
 export interface Certification {
   id: number;
@@ -15,34 +16,49 @@ export interface Certification {
 export const CERTIFICATIONS_DATA: Certification[] = [
   {
     id: 1,
-    title: "AWS Certified Solutions Architect – Associate",
-    issuer: "Amazon Web Services",
-    date: "Dic 2024",
-    credentialId: "AWS-SCA-2024-XYZ",
-    url: "#", // Link a Credly o similar
-    icon: <FaAws />,
-    color: "#FF9900", // AWS Orange
+    title: "Java Spring Boot Microservices eCommerce Project Masterclass",
+    issuer: "Udemy",
+    date: "Dic 2025",
+    credentialId: "UC-07c93e45-1f2e-43c4-aae5-58d1e18f6a16",
+    url: "https://www.udemy.com/certificate/UC-07c93e45-1f2e-43c4-aae5-58d1e18f6a16/",
+    icon: <SiSpring />,
+    color: "#6DB33F",
     skills: [
-      "Arquitectura Cloud Serverless",
-      "Diseño de Microservicios Escalables",
-      "Seguridad y Gestión de IAM",
-      "Despliegue con CI/CD en AWS"
+      "Arquitectura de Microservicios",
+      "JPA, Kafka, RabbitMQ",
+      "Aplicación de Spring Cloud Gateway",
+      "Kubernetes y Docker"
     ]
   },
   {
     id: 2,
-    title: "Meta React Native Specialization",
-    issuer: "Meta (vía Coursera)",
-    date: "Ago 2024",
-    credentialId: "COURSERA-RN-5678",
-    url: "#",
-    icon: <FaReact />,
-    color: "#61DAFB",
+    title: "Build a Backend REST API with Python & Django - Advanced",
+    issuer: "Udemy",
+    date: "Ago 2025",
+    credentialId: "UC-9ed06ecb-1efd-4b01-9d1c-173b10c6b416",
+    url: "https://www.udemy.com/certificate/UC-9ed06ecb-1efd-4b01-9d1c-173b10c6b416/",
+    icon: <SiDjango />,
+    color: "#092E20",
     skills: [
-      "Desarrollo Móvil Multiplataforma",
-      "Gestión de Estado Avanzada",
-      "Integración de APIs Nativas",
-      "UX/UI para Móviles"
+      "Programación Avanzada en Django",
+      "Testing y Depuración",
+      "Despliegue en Producción",
+      "TDD y Buenas Prácticas"
+    ]
+  },
+  {
+    id: 2,
+    title: "Docker & Kubernetes: The Practical Guide",
+    issuer: "Udemy",
+    date: "Jun 2025",
+    credentialId: "UC-f1ffb6dc-d4a7-4523-b128-a30e38627a62",
+    url: "https://www.udemy.com/certificate/UC-f1ffb6dc-d4a7-4523-b128-a30e38627a62/",
+    icon: <FaDocker />,
+    color: "#2494E8",
+    skills: [
+      "AWS ECS",
+      "Desplegue de Contenedores",
+      "Kubernetes Inicialización y Gestión",
     ]
   }
 ];
