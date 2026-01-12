b13f75d42c5d17eef6fd786e2deadc1f33bec134
Luis Fernando Chumbes Ramos <luisfernando3chr@gmail.com>
2026-01-12 00:04:34 -0500

    Fix: Corrección de router

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import { store } from '@store/store'
 import MainLayout from './components/layout/MainLayout';
@@ -7,16 +7,16 @@ import Resume from './pages/Resume';
 
 function App() {
   return (
-    <BrowserRouter>
-     <Provider store={store}>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="resume" element={<Resume />} />
-        </Route>
-      </Routes>
-     </Provider>
-    </BrowserRouter>
+    <HashRouter>
+      <Provider store={store}>
+       <Routes>
+         <Route path="/" element={<MainLayout />}>
+           <Route index element={<Home />} />
+           <Route path="resume" element={<Resume />} />
+         </Route>
+       </Routes>
+      </Provider>
+    </HashRouter>
   );
 }
 
