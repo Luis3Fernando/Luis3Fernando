d21216e06780683acbada1f4523489ee95e35d0b
Luis Fernando Chumbes Ramos <luisfernando3chr@gmail.com>
2026-04-24 21:09:36 -0500

    Refactor: Changes desciption and information of ABOUT_DATA

--- src/data/about.ts
+++ src/data/about.ts
@@ -2,16 +2,14 @@ import { LogoUnamba } from "@assets/icons";
 
 export const ABOUT_DATA = {
   description: [
-    "Estudiante de Ingeniería Informática y de Sistemas especializado en arquitecturas modulares y código limpio. Especializado en desarrollo backend (Spring Boot, Django, .NET Core) y la integración con frontend moderno (Angular, React).",
-    "Participé en el desarrollo de proyectos relevantes como Apugo (app de turismo en Play Store) y Veltu (red social académica), liderando el diseño full stack con enfoque en escalabilidad y rendimiento.",
-    "Me enfoco en arquitecturas seguras y escalables, entregando valor tangible en cada proyecto.",
+    "Bachiller de Ingeniería Informática y de Sistemas enfocado en desarrollo de software con experiencia en Angular, React, .NET y Spring Boot. Especializado en la creación de aplicaciones web escalables y gestión de bases de datos. Comprometido con la responsabilidad técnica y el aprendizaje continuo de nuevas tecnologías para optimizar procesos de desarrollo.",
   ],
   education: {
     university: "Universidad Nacional Micaela Bastidas de Apurímac",
     universityUrl: "https://www.unamba.edu.pe",
     logo: LogoUnamba,
     degree: "Ingeniería Informática y Sistemas",
-    status: "Recién Egresado",
+    status: "Bachiller",
     year: "2020 - 2025",
     location: "Abancay, Apurímac",
     achievements: [
